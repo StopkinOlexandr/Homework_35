0a9c5bc1d4928ff3c3b6bb2fc28bd9c275f369ff
AlexandrStopkin <stnalex1@gmail.com>
2023-03-06 18:50:04 +0100

    Add two more instances

--- src/Homework_35.java
+++ src/Homework_35.java
@@ -1,5 +1,15 @@
 public class Homework_35 {
   public static void main(String[] args) {
     Student ivanov = new Student();
+    ivanov.name = "Вася Иванов";
+    ivanov.number = 1;
+
+    Student petrov = new Student();
+    petrov.name = "Иван Петров";
+    petrov.number = 2;
+
+    Student sidorov = new Student();
+    sidorov.name = "Петр Сидоров";
+    sidorov.number = 3;
   }
 }
